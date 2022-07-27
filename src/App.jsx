1e8f3a6a41977771dbbd741bb7ccce6242eb21a7
Catharine Augusto <catharine.augusto@gmail.com>
2022-07-26 23:10:03 -0300

    footer e fontes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/section/navbar/Navbar';
 import Header from './components/section/header/Header';
 import About from './components/section/about/About';
+import Footer from './components/element/footer/Footer';
 
 function App() {
   
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <Navbar />    
       <Header />
-      <About />      
+      <About />
+      <Footer />      
     </div>
   )
 }
---
+++ src/components/element/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+
+        <footer className="footer">
+            <p>
+                Desenvolvido em <strong>React</strong> por {' '}
+                <b>
+                    <a
+                        href="https://www.linkedin.com/in/catharine-augusto/"
+                        target="blank">
+                        Catharine Augusto
+                    </a>
+                </b>
+            </p>
+        </footer>
+
+    )
+}
+
+export default Footer
